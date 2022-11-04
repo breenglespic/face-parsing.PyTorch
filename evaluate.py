87b336f2b346b29c47e684421019e912093942b5
Artem Kotov <kotov.artem@picsart.com>
2022-11-04 13:15:14 +0400

    blacked

--- evaluate.py
+++ evaluate.py
@@ -22,17 +22,35 @@ from PIL import Image
 import torchvision.transforms as transforms
 import cv2
 
-def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
+
+def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path="vis_results/parsing_map_on_im.jpg"):
     # Colors for all 20 parts
-    part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
-                   [255, 0, 85], [255, 0, 170],
-                   [0, 255, 0], [85, 255, 0], [170, 255, 0],
-                   [0, 255, 85], [0, 255, 170],
-                   [0, 0, 255], [85, 0, 255], [170, 0, 255],
-                   [0, 85, 255], [0, 170, 255],
-                   [255, 255, 0], [255, 255, 85], [255, 255, 170],
-                   [255, 0, 255], [255, 85, 255], [255, 170, 255],
-                   [0, 255, 255], [85, 255, 255], [170, 255, 255]]
+    part_colors = [
+        [255, 0, 0],
+        [255, 85, 0],
+        [255, 170, 0],
+        [255, 0, 85],
+        [255, 0, 170],
+        [0, 255, 0],
+        [85, 255, 0],
+        [170, 255, 0],
+        [0, 255, 85],
+        [0, 255, 170],
+        [0, 0, 255],
+        [85, 0, 255],
+        [170, 0, 255],
+        [0, 85, 255],
+        [0, 170, 255],
+        [255, 255, 0],
+        [255, 255, 85],
+        [255, 255, 170],
+        [255, 0, 255],
+        [255, 85, 255],
+        [255, 170, 255],
+        [0, 255, 255],
+        [85, 255, 255],
+        [170, 255, 255],
+    ]
 
     im = np.array(im)
     vis_im = im.copy().astype(np.uint8)
@@ -56,7 +74,8 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     # return vis_im
 
-def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth'):
+
+def evaluate(respth="./res/test_res", dspth="./data", cp="79999_iter.pth"):
 
     if not os.path.exists(respth):
         os.makedirs(respth)
@@ -64,14 +83,16 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
     net.cuda()
-    save_pth = osp.join('res/cp', cp)
+    save_pth = osp.join("res/cp", cp)
     net.load_state_dict(torch.load(save_pth))
     net.eval()
 
-    to_tensor = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
+    to_tensor = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
     with torch.no_grad():
         for image_path in os.listdir(dspth):
             img = Image.open(osp.join(dspth, image_path))
@@ -85,11 +106,6 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
             vis_parsing_maps(image, parsing, stride=1, save_im=True, save_path=osp.join(respth, image_path))
 
 
-
-
-
-
-
 if __name__ == "__main__":
-    setup_logger('./res')
+    setup_logger("./res")
     evaluate()
